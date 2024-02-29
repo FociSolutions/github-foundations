2d876690ec4c47861b9ea1c2bef6ddbf07c7d54d
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-02-29 09:35:48 -0500

    deleted modules folder in organization layer and updated readme to reflect changes

--- organizations/modules/organization/members.tf
+++
@@ -1,6 +0,0 @@
-resource "github_membership" "membership_for_user" {
-  for_each = toset(var.github_organization_members)
-
-  username = each.value
-  role     = "member"
-}
--- organizations/modules/team_set/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "team_slugs" {
-  value = {
-    for _, team in module.team : team.name => team.slug
-  }
-  description = "Map of team names to their respective slugs"
-}
