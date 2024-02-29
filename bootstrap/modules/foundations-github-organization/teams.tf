f4daa5d1b3ceb2a301b2b89a6a1bbd149e6007b9
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-02-29 09:35:30 -0500

    deleted modules folder and changed reference in bootstrap layer

--- bootstrap/modules/foundations-github-organization/teams.tf
+++
@@ -1,7 +0,0 @@
-resource "github_team" "foundation_devs" {
-  provider = github.foundation_org_scoped
-
-  name        = "foundation-devs"
-  description = "Team members with write access to the foundation repositories"
-  privacy     = "closed"
-}
--- bootstrap/modules/github-gcloud-oidc/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  project_parent = var.project_parent == null ? google_folder.folder[0].id : var.project_parent
-}
