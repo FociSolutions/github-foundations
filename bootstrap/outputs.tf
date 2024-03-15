cafe12edfd8fce1cd4f1ec30418d251732fa8546
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-03-15 13:45:02 -0400

    modified bootstrap tf files

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,4 +1,4 @@
 output "foundation_dev_team_id" {
   description = "The ID of the foundation-devs team in the foundation organization."
-  value = module.foundations_github_organization.foundation_dev_team_id
+  value = module.github_foundations.foundation_dev_team_id
 }
